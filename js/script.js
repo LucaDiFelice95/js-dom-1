b3b232f7b91d2f021c7c84845739665fac678809
Luca Di Felice <lucadifelice95@gmail.com>
2026-06-02 17:27:29 +0200

    Logic Code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+// ESTRAGGO DAL DOM LE CLASSI CHE MI OCCORRONO E LE INSERISCO IN DELLE VARIABILI
+const lamp = document.querySelector(".lamp");
+const btn = document.querySelector(".btn");
+// STATO DELLA LAMPADINA
+let activeState = 0;
+
+//COSTRUISCO UNA FUNZIONE DI ACCESNIONE E SPEGNIMENTO
+const turnOn = function () {
+  // SE LO STATO DELLA LAMPADINA è SPENTO
+  if (activeState === 0) {
+    //SOSTITUISCO L'IMMAGINE ALLA CLASSE .lamp
+    lamp.src = "../assets/img/yellow_lamp.png";
+    // CAMBIO LO STATO DELLA VARIABILE DA 0 A 1
+    activeState = 1;
+    // SCRIVO SUL BOTTONE 'SPEGNI'
+    btn.textContent = "Spegni";
+  } else {
+    // SOSTITUISCO L'IMMAGINE ALLA CLASSE .lamp
+    lamp.src = "../assets/img/white_lamp.png";
+    // CAMBIO LO STATO DELLA VARIABILE DA 0 A 1
+    activeState = 0;
+    // SCRIVO SUL BOTTONE 'ACCENDI'
+    btn.textContent = "Accendi";
+  }
+};
+
+btn.addEventListener("click", turnOn);
